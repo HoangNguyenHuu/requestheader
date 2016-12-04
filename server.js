5d0f08fdad2610854e5d1cef8bebe587da04232f
Nguyen Huu Hoang <hoangnguyenhuu94@gmail.com>
2016-12-04 13:21:34 +0000

    complete request header

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/whoami', function (req, res) {
+//   res.send(req.headers);
+  var ip = req.headers['x-forwarded-for'];
+  var language = req.headers["accept-language"].split(',')[0];
+  var software = req.headers['user-agent'];
+  var index1 = software.indexOf('(');
+  var index2 = software.indexOf(')');
+//   console.log(index1 + " " + index2);
+  software = software.substring(index1 +1, index2);
+  var result = {
+      "ip": ip,
+      "language": language,
+      "software": software
+  };
+  res.send(result);
 })
 
 app.listen(8080, function () {
